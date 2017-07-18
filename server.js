54714f4e2c67bd1770f50a35407f2e37c8e790de
andrew.nelsonrd@gmail.com <andrew.nelsonrd@gmail.com>
2017-07-18 06:25:21 -0700

    cors nt workin.

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ var port = Number(process.env.PORT || 8080);
 
 app.use(json())
 
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
 app.use(cors());
 
 app.listen(port, function () {
